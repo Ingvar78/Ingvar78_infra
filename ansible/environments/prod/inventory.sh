8a7add4bc7d7415bb1df271e685c63c404a10c60
Igor V <igor@eger.pro>
2020-07-25 23:27:55 +0300

    Ansible-3 p41

---
+++ ansible/environments/prod/inventory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" = "--list" ]; then
+    cd ../../../terraform/prod > /dev/null
+    terraform output inventory
+    cd - > /dev/null
+else
+    cat << _EOF_
+{
+    "_meta": {
+            "hostvars": {}
+    },
+    "all": {
+            "children": [
+                    "ungrouped"
+            ]
+    },
+    "ungrouped": {}
+}
+_EOF_
+fi
